b2545f04e90b34629b3eb0ae20b998e2c12af695
SuperiorM <imam.fadhil28@gmail,com>
2026-08-31 17:34:10 +0800

    fix : changing background

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,9 +1,11 @@
-import * as admin from 'firebase-admin';
+import { getApps, initializeApp, cert } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+import { getAuth } from 'firebase-admin/auth';
 
-if (!admin.apps.length) {
+if (!getApps().length) {
   try {
-    admin.initializeApp({
-      credential: admin.credential.cert({
+    initializeApp({
+      credential: cert({
         projectId: process.env.FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // Mengganti literal \n dengan karakter baris baru agar private key bisa terbaca
@@ -16,7 +18,7 @@ if (!admin.apps.length) {
   }
 }
 
-const adminDb = admin.firestore();
-const adminAuth = admin.auth();
+const adminDb = getFirestore();
+const adminAuth = getAuth();
 
 export { adminDb, adminAuth };
